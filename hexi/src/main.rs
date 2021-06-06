ef1a86846d2bf72071be42bcbcb116cd8d493cfc
Aaron <aarlin5409@hessleacademy.com>
2021-06-06 11:17:03 +0100

    added sigpipe handling and changed CI

--- hexi/src/main.rs
+++ hexi/src/main.rs
@@ -1,6 +1,19 @@
 use clap::{load_yaml, value_t_or_exit, App};
 use hexi_lib::{error::Error, hexi::Hexi, options::Options};
 
+/// If on unix, ensure the SIGPIPE signal doesn't cause program to panic - this allows pipes to be used with this program.
+#[cfg(unix)]
+fn reset_sigpipe() {
+    unsafe {
+        libc::signal(libc::SIGPIPE, libc::SIG_DFL);
+    }
+}
+
+#[cfg(not(unix))]
+fn reset_sigpipe() {
+    // no-op
+}
+
 /// Gets the options this program was ran with.
 fn get_options() -> Options {
     let yaml = load_yaml!("../cli.yml");
@@ -23,6 +36,7 @@ fn run(options: Options) -> Result<(), Error> {
 }
 
 fn main() {
+    reset_sigpipe();
     let options = get_options();
 
     if let Err(e) = run(options) {
